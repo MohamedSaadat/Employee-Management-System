d27bef6f862dce27013d7f00be07c8c5efc9fa62
MohamedSaatat <mohamedgitsaadat@gmail.com>
2026-08-28 21:58:06 +0300

    Create Sidebar

---
+++ client/src/App.jsx
@@ -0,0 +1,55 @@
+import { Toaster } from "react-hot-toast";
+import { Navigate, Route, Routes } from "react-router-dom";
+import LoginLanding from "./pages/LoginLanding";
+import Layout from "./pages/Layout";
+import Attendance from "./pages/Attendance";
+import Dashboard from "./pages/Dashboard";
+import Employees from "./pages/Employees";
+import Leave from "./pages/Leave";
+import Payslips from "./pages/Payslips";
+import PrintPayslip from "./pages/PrintPayslip";
+import Setting from "./pages/Setting";
+import LoginForm from "./components/LoginForm";
+
+const App = () => {
+  return (
+    <>
+      <Toaster />
+      <Routes>
+        <Route path="/login" element={<LoginLanding />} />
+        <Route
+          path="/login/admin"
+          element={
+            <LoginForm
+              role="admin"
+              title="Admin Portal"
+              subtitle="Sign in to manage the organization"
+            />
+          }
+        />
+        <Route
+          path="/login/employee"
+          element={
+            <LoginForm
+              role="employee"
+              title="Employee Portal"
+              subtitle="Sign in to access your account"
+            />
+          }
+        />
+        <Route element={<Layout />}>
+          <Route path="/attendance" element={<Attendance />} />
+          <Route path="/dashboard" element={<Dashboard />} />
+          <Route path="/employees" element={<Employees />} />
+          <Route path="/leave" element={<Leave />} />
+          <Route path="/payslips" element={<Payslips />} />
+          <Route path="/setting" element={<Setting />} />
+        </Route>
+        <Route path="/print/Payslip/:id" element={<PrintPayslip />} />
+        <Route path="*" element={<Navigate to="/dashboard" replace />} />
+      </Routes>
+    </>
+  );
+};
+
+export default App;
